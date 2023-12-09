950e81a5212e8735234f17739708dd131d267244
apple <apple>
2023-12-09 06:32:06 +0500

    added the wallet connect

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {
+  createWeb3Modal,
+  defaultConfig,
+  useWeb3Modal,
+  useWeb3ModalProvider,
+  useWeb3ModalAccount,
+  useDisconnect,
+} from "@web3modal/ethers/react";
+import { BrowserProvider } from "ethers";
 
-function App() {
+const PROJECT_ID = "461a5c215e7994c3281f94e8b71e4426";
+
+const mainnet = {
+  chainId: 1,
+  name: "Ethereum",
+  currency: "ETH",
+  explorerUrl: "https://etherscan.io",
+  rpcUrl: "https://cloudflare-eth.com",
+};
+
+const metadata = {
+  name: "My Website",
+  description: "My Website description",
+  url: "https://mywebsite.com",
+  icons: ["https://avatars.mywebsite.com/"],
+};
+
+createWeb3Modal({
+  ethersConfig: defaultConfig({ metadata }),
+  chains: [mainnet],
+  projectId: PROJECT_ID,
+});
+
+const App = () => {
+  const { isConnected } = useWeb3ModalAccount();
+  const { walletProvider } = useWeb3ModalProvider();
+  const { open } = useWeb3Modal();
+  const { disconnect } = useDisconnect();
+
+  async function onClickWalletConnect() {
+    open();
+  }
+
+  async function getSigner() {
+    if (!isConnected) {
+      return console.log("user is not connected");
+    }
+    const provider = new BrowserProvider(walletProvider);
+    const signer = await provider.getSigner();
+    const address = await signer.getAddress();
+    console.log("address", address);
+  }
+
+  async function onDisconnect() {
+    if (!isConnected) {
+      return console.log("user is not connected");
+    }
+    disconnect();
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button onClick={onClickWalletConnect}>Connect Wallet Connect</button>
+      <button onClick={getSigner}>Get Signer</button>
+      <button onClick={onDisconnect}>Disconnect</button>
     </div>
   );
-}
+};
 
 export default App;
